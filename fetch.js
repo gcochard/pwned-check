c4e7075e45801639025e0216e780e5d4d5b6c43e
Greg Cochard <gcochard@google.com>
2017-08-24 16:51:47 -0700

    Fix fetch for linux

--- fetch.js
+++ fetch.js
@@ -16,56 +16,76 @@ const async = require('async');
 const request = require('request');
 const fs = require('fs');
 const path = require('path');
+const os = require('os');
 const { spawn } = require('child_process');
 const lists = ['https://downloads.pwnedpasswords.com/passwords/pwned-passwords-1.0.txt.7z', 'https://downloads.pwnedpasswords.com/passwords/pwned-passwords-update-1.txt.7z', 'https://downloads.pwnedpasswords.com/passwords/pwned-passwords-update-2.txt.7z'];
+const unzipCmds = {
+  'darwin': {binary:'7z', args:['x', '-y']},
+  'linux': {binary:'p7zip', args:['-d']}
+};
+const platform = os.platform();
+// default to linux
+const unzipCmd = unzipCmds[platform] || unzipCmds.linux;
 const fetcher = (listUrl, cb) => {
-  console.log(`fetching ${listUrl} ...`);
-  let filename = path.basename(listUrl), rs;
-  rs = fs.createWriteStream(filename, {flags: 'wx'});
-  const decompress = () => {
-    const decompressed = path.basename(filename, path.extname(filename));
-    fs.stat(decompressed, (err, stats) => {
-      if(err && err.code == 'ENOENT'){
-        console.log(`unzipping ${filename} ...`);
-        let child = spawn('7z', ['x', '-y', filename]);
-        child.on('error', err => {
-          console.error(err);
-          return cb(err);
-        });
-        child.on('close', code => {
-          console.log(`finished unzipping ${filename}`);
+  const filename = path.basename(listUrl);
+  const decompressed = path.basename(filename, path.extname(filename));
+  fs.stat(decompressed, (err, stats) => {
+    if(!err || err.code != 'ENOENT'){
+      console.log(`${decompressed} exists, skipping download and decompression`);
+      return cb();
+    }
+    console.log(`fetching ${listUrl} ...`);
+    let rs = fs.createWriteStream(filename, {flags: 'wx', defaultEncoding: 'binary', mode: 0o644});
+    const decompress = () => {
+      const decompressed = path.basename(filename, path.extname(filename));
+      fs.stat(decompressed, (err, stats) => {
+        if(err && err.code == 'ENOENT'){
+          console.log(`unzipping ${filename} ...`);
+          let child = spawn(unzipCmd.binary, [...unzipCmd.args, filename]);
+          child.on('error', err => {
+            if(err.code == 'ENOENT'){
+              return cb(new Error(`Error, 7z binary: ${unzipCmd.binary} not found. Please ensure it is installed and in your PATH`));
+            }
+            return cb(err);
+          });
+          child.on('close', code => {
+            console.log(`finished unzipping ${filename}`);
+            return cb();
+          });
+          child.stdout.pipe(process.stdout);
+          child.stderr.pipe(process.stderr);
+        } else {
+          console.log(`${decompressed} exists, skipping decompression`);
           return cb();
-        });
-        child.stdout.pipe(process.stdout);
-        child.stderr.pipe(process.stderr);
-      } else {
-        console.log(`${decompressed} exists, skipping decompression`);
-        return cb();
+        }
+      });
+    };
+    let rq = request(listUrl);
+    let skipped = false;
+    rs.on('error', e => {
+      if(e.code == 'EEXIST'){
+        console.log(`warning: ${filename} already exists, skipping download...`);
+        skipped = true;
+        return decompress();
       }
     });
-  };
-  let rq = request(listUrl);
-  let skipped = false;
-  rs.on('error', e => {
-    if(e.code == 'EEXIST'){
-      console.log(`warning: ${filename} already exists, skipping download...`);
-      skipped = true;
-      return decompress();
-    }
-  });
-  rq.on('error', err => {
-    console.error(err);
-  }).on('end', () => {
-    if(!skipped){
-      decompress();
-    }
-  });
-  // only fire the request if the file doesn't exist
-  fs.stat(filename, (err, stats) => {
-    if(err && err.code == 'ENOENT'){
-      console.log('firing request');
-      rq.pipe(rs);
-    }
+    rq.on('error', err => {
+      console.error(err);
+    }).on('data', chunk => {
+      rs.write(chunk);
+    }).on('end', () => {
+      rs.end();
+      if(!skipped){
+        decompress();
+      }
+    });
+    // only fire the request if the file doesn't exist
+    fs.stat(filename, (err, stats) => {
+      if(err && err.code == 'ENOENT'){
+        console.log('firing request');
+        rq.pipe(rs);
+      }
+    });
   });
 };
 
