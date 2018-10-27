6a710d19f1694c9501dacd8e27bce3a1ba1aebf7
Greg Cochard <gcochard@users.noreply.github.com>
2018-10-27 13:59:48 -0700

    Add license

--- pwned-alert.user.js
+++ pwned-alert.user.js
@@ -1,7 +1,21 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // ==UserScript==
 // @name         Pwned alert
 // @namespace    http://gcochard.github.io/pwned-check/pwned-alert.user.js
-// @version      1.0
+// @version      1.0.1
 // @description  Check password fields against the pwned password API using k-anonymity and report how many times it's been pwned.
 // @author       Greg Cochard
 // @match        *://*/*
